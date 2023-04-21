606448e58f26e2fa9bd4f8d296646c79324db4bb
poipiii <nanosonico1@gmail.com>
2023-04-21 15:19:44 +0800

    [feat] added org,catergory,and doc page

--- dev/src/App.tsx
+++ dev/src/App.tsx
@@ -1,6 +1,9 @@
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { HashRouter, Routes, Route } from 'react-router-dom';
-import Home from './Home';
+import Home from './pages/Home';
+import Organizations from './pages/Organizations';
+import Category from './pages/Catergory';
+import Documents from './pages/Documents';
 import './App.css';
 
 // Create client
@@ -11,7 +14,12 @@ function App() {
     <HashRouter>
       <QueryClientProvider client={queryClient}>
         <Routes>
-          <Route path='/' element={<Home />} />
+          {/* <Route path='/' element={<Home />} /> */}
+          {/* <Route path='/organizations' element={<Organizations />} /> */}
+          {/* <Route path='/category' element={<Category />} /> */}
+          <Route path='/' element={<Documents />} />
+
+
         </Routes>
       </QueryClientProvider>
     </HashRouter>
--- dev/src/pages/Home/index.tsx
+++ dev/src/pages/Home/index.tsx
@@ -2,9 +2,9 @@
 
 // Custom imports - hooks, utilities, components, configs, then styles
 
-import ClippedDrawer from "../components/ClippedDrawer"
-import DocumentCard from "../components/ui/DocumentCard";
-import OrgansationCard from "../components/ui/OrgansationCard";
+import ClippedDrawer from "../../components/ClippedDrawer"
+import DocumentCard from "../../components/ui/DocumentCard";
+import OrgansationCard from "../../components/ui/OrgansationCard";
 import MergeItem from "@/components/ui/MergeItem";
 import NavBar from "@/components/ui/NavBar";
 import Footer from "@/components/ui/Footer";
